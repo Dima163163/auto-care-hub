ed1e0b85a31fa9c2c09a1ed95617068389bdb828
Dima163163 <khlopovdima@gmail.com>
2026-08-25 20:48:52 +0400

    fix: allow vite fallback origin in development

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -506,8 +506,10 @@ function getCorsOrigins(nodeEnv: NodeEnv, defaultOrigin: string) {
         ...configuredOrigins,
         'http://localhost:5173',
         'http://localhost:5174',
+        'http://localhost:5178',
         'http://127.0.0.1:5173',
         'http://127.0.0.1:5174',
+        'http://127.0.0.1:5178',
     ])]
 }
 
